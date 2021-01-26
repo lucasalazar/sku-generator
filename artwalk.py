566e57497b4921ccae97c5e4726f0a9dd4ba5a79
Luca Salazar <43450396+lucasalazar@users.noreply.github.com>
2021-01-26 17:06:41 -0300

    Create artwalk.py

---
+++ artwalk.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup as bs #Importa pacotes necessários
+import requests
+import webbrowser
+
+arrayLinks = []
+
+def get_links():
+    url = "https://www.artwalk.com.br/checkout/cart/add?sku="
+    url2 = "&qty=1&seller=1&redirect=false&sc=1"
+    sku = 2136283
+    global arrayLinks
+    links = open("C:\Users\lucas\Desktop\SKU ARTWALK\links.txt", "w+")
+    while(sku < 2139000):
+        urlCompleto = url + str(sku) + url2
+        arrayLinks.append(urlCompleto)
+        sku+=1
+        links.write(sku+"\n")
+        
+get_links()
